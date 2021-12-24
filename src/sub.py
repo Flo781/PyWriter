a016b290986805605cc81dda521ac22657bfcd7d
Flo_781 <81481365+Flo781@users.noreply.github.com>
2021-12-24 20:37:22 +0100

    Update sub.py

--- src/sub.py
+++ src/sub.py
@@ -16,7 +16,25 @@ class main:
                          \▓▓      _\▓▓▓▓▓▓▓\▓▓      \▓▓\▓▓      \▓▓   \▓▓▓▓  \▓▓▓▓▓▓▓\▓▓      
                                 |  \__| ▓▓                                                   
                                 \▓▓    ▓▓                                                   
-                                 \▓▓▓▓▓▓                                                                                                    
+                                 \▓▓▓▓▓▓ 
+     .-~~\
+           /     \ _
+           ~x   .-~_)_
+             ~x".-~   ~-.
+         _   ( /         \   _
+         ||   T  o  o     Y  ||
+       ==:l   l   <       !  I;==
+          \\   \  .__/   /  //
+           \\ ,r"-,___.-'r.//
+            }^ \.( )   _.'//.
+           /    }~Xi--~  //  \
+          Y    Y I\ \    "    Y
+          |    | |o\ \        |
+          |    l_l  Y T       |  -Row
+          l      "o l_j       !
+           \                 /
+    ___,.---^.     o       .^---.._____
+"~~~          "           ~            ~~~"                                                                                              
 """, style="#ffa407")
         from colorama import Fore, Style
         print(Fore.CYAN + "\t\t\t\t\t\t\t\t made by " + Fore.LIGHTMAGENTA_EX + "@Flo_781" + Fore.CYAN + ", " + Fore.LIGHTMAGENTA_EX + "@skyline69\n\n\n" + Style.RESET_ALL)
